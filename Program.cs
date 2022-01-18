4d033176b8d34b973ea38ef6901440699535aa16
msla-InspireSA <80754340+msla-InspireSA@users.noreply.github.com>
2022-01-18 18:38:14 +0200

    edited code

--- Program.cs
+++ Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json; // Install Newtonsoft.Json with NuGet
 
-namespace textransaltor
+class Program
 {
-    internal class Program
+    private static readonly string subscriptionKey = "Get your own key";
+    private static readonly string endpoint = "https://api.cognitive.microsofttranslator.com/";
+
+    // Add your location, also known as region. The default is global.
+    // This is required if using a Cognitive Services resource.
+    private static readonly string location = "westeurope";
+
+    static async Task Main(string[] args)
     {
-        static void Main(string[] args)
+        // Input and output languages are defined as parameters.
+        string route = "/translate?api-version=3.0&from=en&to=de&to=hr";
+        string textToTranslate = "Hello, world!";
+        object[] body = new object[] { new { Text = textToTranslate } };
+        var requestBody = JsonConvert.SerializeObject(body);
+
+        using (var client = new HttpClient())
+        using (var request = new HttpRequestMessage())
         {
-            Console.WriteLine("Hello World!");
+            // Build the request.
+            request.Method = HttpMethod.Post;
+            request.RequestUri = new Uri(endpoint + route);
+            request.Content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+            request.Headers.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+            request.Headers.Add("Ocp-Apim-Subscription-Region", location);
+
+            // Send the request and get response.
+            HttpResponseMessage response = await client.SendAsync(request).ConfigureAwait(false);
+            // Read response as a string.
+            string result = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(result);
         }
     }
 }
